8937d77fc942ec8585ea2e4c59472f1cc8de9619
hammone2 <hammone2@my.erau.edu>
2025-10-09 11:55:27 -0700

    Fix Task Disabled Bug

--- Scurvy Seas/Assets/Scripts/Task.cs
+++ Scurvy Seas/Assets/Scripts/Task.cs
@@ -1,5 +1,7 @@
 using UnityEngine;
 using UnityEngine.Events;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 
 [RequireComponent(typeof(Collider))]
 [RequireComponent(typeof(Rigidbody))] //so the player can collide with the task (the ship will override the collision without this rb)
@@ -9,6 +11,8 @@ public class Task : MonoBehaviour
 
     public UnityEvent OnEnableTask;
     public UnityEvent OnDisableTask;
+    private Crewmate crewmateWorkingThisTask;
+    private List<Crewmate> crewmatesInArea = new List<Crewmate>(); 
 
     private bool _isManned;
     private bool isManned
@@ -55,20 +59,50 @@ public class Task : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.layer == LayerMask.NameToLayer("Crewmate") && isManned == false)
+        if (other.gameObject.layer == LayerMask.NameToLayer("Crewmate"))
         {
-            isManned = true;
-            other.GetComponent<Crewmate>().isDoingTask = isManned;
+            Crewmate crewmate = other.GetComponent<Crewmate>();
+            
+            if (crewmateWorkingThisTask == null) //is nobody working this task?
+                SetManned(true, crewmate);
+
+            crewmatesInArea.Add(crewmate);
+            Debug.Log(crewmatesInArea.Count);
         }
     }
 
     private void OnTriggerExit(Collider other)
     {
-        if (other.gameObject.layer == LayerMask.NameToLayer("Crewmate") && isManned == true)
+        if (other.gameObject.layer == LayerMask.NameToLayer("Crewmate"))
         {
-            isManned = false;
-            other.GetComponent<Crewmate>().isDoingTask = isManned;
+            Crewmate crewmate = other.GetComponent<Crewmate>();
+            
+            if (crewmateWorkingThisTask == crewmate) //did our crewmate working the task leave?
+                SetManned(false, crewmate);
+
+            crewmatesInArea.Remove(crewmate);
+            Debug.Log(crewmatesInArea.Count);
+
+            if (crewmatesInArea.Count == 0)
+                return;
+                
+            //have next crewmate in the area man the task station
+            Crewmate newCrewmate = crewmatesInArea[0];
+            SetManned(true, newCrewmate);
         }
+    }
+
+    private void SetManned(bool _isManned, Crewmate _crewmate)
+    {
+        isManned = _isManned;
+        _crewmate.isDoingTask = _isManned;
+
+        if (!_isManned)
+        {
+            crewmateWorkingThisTask = null;
+            return;
+        }
+        crewmateWorkingThisTask = _crewmate;
     }
 
     public bool GetIsManned()
